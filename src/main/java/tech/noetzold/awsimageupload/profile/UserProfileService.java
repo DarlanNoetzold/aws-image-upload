5121a334dfe832c09b67dc0685cafb67dd280ed9
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-05-16 06:31:02 -0300

    Update UserProfileService.java

--- src/main/java/tech/noetzold/awsimageupload/profile/UserProfileService.java
+++ src/main/java/tech/noetzold/awsimageupload/profile/UserProfileService.java
@@ -29,18 +29,13 @@ public class UserProfileService {
     }
 
     void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
-        // 1. Check if image is not empty
         isFileEmpty(file);
-        // 2. If file is an image
         isImage(file);
 
-        // 3. The user exists in our database
         UserProfile user = getUserProfileOrThrow(userProfileId);
 
-        // 4. Grab some metadata from file if any
         Map<String, String> metadata = extractMetadata(file);
 
-        // 5. Store the image in s3 and update database (userProfileImageLink) with s3 image link
         String path = String.format("%s/%s", BucketName.PROFILE_IMAGE.getBucketName(), user.getUserProfileId());
         String filename = String.format("%s-%s", file.getOriginalFilename(), UUID.randomUUID());
 
