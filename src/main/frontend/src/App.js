158fec14dee997dd97606d619a6156a0b0cbb23d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-05-17 08:09:25 -0300

    Update App.js

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,10 +1,27 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+
+
+const UserProfile = () =>{
+    const fetchUserProfiles = () => {
+      axios.get("http://localhost:8080/api/v1/user-profile").then(res =>{
+        console.log(res);
+      });
+    }
+
+    useEffect(() => {
+      fetchUserProfiles();
+    }, []);
+
+    return <h1>Hello</h1>;
+}
 
 function App() {
   return (
     <div className="App">
-      
+      <UserProfile />
     </div>
   );
 }
