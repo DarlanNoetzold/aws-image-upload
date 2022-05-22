661e4aa6ec135f693c1cd534d1dd22fef0e5891b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-05-22 07:47:39 -0300

    Update App.js

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -24,17 +24,36 @@ const UserProfile = () =>{
 
         return (
         <div key={index}>
-          <Dropzone />
+          {userProfiles.userProfileId ? <img src={`http://localhost:8080/api/v1/user-profile/${userProfiles.userProfileId}/image/download`}/> : null}
           <h1>{userProfiles.username}</h1>
           <p>{userProfiles.userProfileId}</p>
+          <Dropzone {...userProfile}/>
         </div>
         );
       });
 
-      function Dropzone() {
+      function Dropzone({ userProfileId }) {
         const onDrop = useCallback(acceptedFiles => {
           const file = acceptedFiles[0];
+          
           console.log(file);
+          
+          const formData = new FormData();
+          formData.append("file", file)
+
+          axios.post(
+            `http://localhost:8080/api/v1/user-profile/${userProfileId}/image/upload`,
+            formData,
+            {
+              headers:{
+                "Content-Type": "multipart/form-data"
+              }
+            }            
+            ).then(() =>{
+              console.log("file uploaded")
+            }).catch(err =>{
+              console.log("file uploaded error" + err)
+            });
         }, [])
         const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
       
